c7f024db0419c45b2358b0358edfe59372209916
Ethan Li <ethanlimucheng@gmail.com>
2024-12-09 19:32:25 +0100

    Initial Commit

---
+++ TicTacToeAI/TicTacToeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAIApp.swift
+//  TicTacToeAI
+//
+//  Created by MuMu on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
